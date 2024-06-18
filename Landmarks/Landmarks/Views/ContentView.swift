8792c2b3834962b19b7a29888cfcd99ae850b0c6
KoheiKanagu <kanagu@kingu.dev>
2024-06-18 13:50:09 +0900

    Chapter 1

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by kingu on 2024/06/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
