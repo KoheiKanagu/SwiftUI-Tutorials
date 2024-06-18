abbbf2a6c3ace9d8898c15eea345d422a7face48
KoheiKanagu <kanagu@kingu.dev>
2024-06-18 09:32:09 +0900

    Chapter 1

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by kingu on 2024/06/17.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+                
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
